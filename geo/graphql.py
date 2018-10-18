9ece10a270cec4037ce3853c626ce33a69b41c42
Jozef Sukovsky <sukovskyj@gmail.com>
2018-10-18 21:11:05 +0200

    added geo types

--- geo/graphql.py
+++ geo/graphql.py
@@ -3,8 +3,63 @@ Geo GraphQL Queries
 """
 
 import graphene
+from graphene_django import DjangoObjectType
 
+from graphql_utils import CountableConnectionBase, OrderedDjangoFilterConnectionField
+
+from geo.models import Region, District, Village
+
+
+class RegionType(DjangoObjectType):
+
+    """SK Regions"""
+
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+        model = Region
+        connection_class = CountableConnectionBase
+        filter_fields = {
+            'shortcut': ['exact'],
+        }
+
+
+class DistrictType(DjangoObjectType):
+
+    """SK Districts"""
+
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+        model = District
+        connection_class = CountableConnectionBase
+        filter_fields = {
+            'name': ['exact', 'icontains'],
+            'region__name': ['exact', 'icontains'],
+            'region__shortcut': ['exact'],
+        }
+
+
+class VillageType(DjangoObjectType):
+
+    """SK Villages"""
+
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+        model = Village
+        connection_class = CountableConnectionBase
+        filter_fields = {
+            'full_name': ['exact', 'icontains'],
+            'district__name': ['exact', 'icontains'],
+            'district__region__name': ['exact', 'icontains'],
+            'district__region__shortcut': ['exact'],
+        }
 
 class GeoQueries(graphene.ObjectType):
 
-    pass
+    region = graphene.relay.Node.Field(RegionType)
+    all_regions = OrderedDjangoFilterConnectionField(RegionType)
+
+    district = graphene.relay.Node.Field(DistrictType)
+    all_districts = OrderedDjangoFilterConnectionField(DistrictType)
+
+    village = graphene.relay.Node.Field(VillageType)
+    all_villages = OrderedDjangoFilterConnectionField(VillageType)
