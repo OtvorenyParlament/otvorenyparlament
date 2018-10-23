71916f479d360f460de33cbf006f6f49a06f6a17
Jozef Sukovsky <sukovskyj@gmail.com>
2018-10-23 10:21:04 +0200

    enabled django-debug-toolbar

--- otvorenyparlament/settings/dev.py
+++ otvorenyparlament/settings/dev.py
@@ -5,6 +5,7 @@ Dev server settings
 from otvorenyparlament.settings.base import *
 
 ALLOWED_HOSTS = ['*']
+INTERNAL_IPS = ['127.0.0.1']
 DEBUG = True
 
 DATABASES = {
--- otvorenyparlament/urls.py
+++ otvorenyparlament/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
@@ -23,3 +25,10 @@ urlpatterns = [
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
