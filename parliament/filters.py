f288762249704f727ea152f1ba2543218a3e29b9
Jozef Sukovsky <sukovskyj@gmail.com>
2018-10-23 10:22:02 +0200

    MemberFilterSet

--- parliament/filters.py
+++ parliament/filters.py
@@ -2,9 +2,33 @@
 Parliament filters
 """
 
+from django.db.models import Q
+from django.utils import timezone
 import django_filters
 
-from parliament.models import VotingVote
+from parliament.models import Member, MemberChange, Period, VotingVote
+
+
+class MemberFilterSet(django_filters.FilterSet):
+
+    is_active = django_filters.DateFilter(field_name='is_active', method='filter_is_active')
+
+    def filter_is_active(self, queryset, name, value):
+        queryset = queryset.filter(
+            Q(active__start__lte=value),
+            Q(active__end__gt=value) | Q(active__end__isnull=True)
+        )
+        return queryset
+
+
+    class Meta:
+        model = Member
+        fields = {
+            'id': ('exact',),
+            'stood_for_party': ('exact',),
+            'period__period_num': ('exact',),
+            'person__id': ('exact',)
+        }
 
 
 class VotingVoteFilterSet(django_filters.FilterSet):
